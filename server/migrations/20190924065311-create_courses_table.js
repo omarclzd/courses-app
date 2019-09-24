017d21e8be6eb2873b4926a2104b77733f0a50f6
omar <omasrt@gmail.com>
2019-09-24 02:12:02 -0500

    makes sequelize migrations to create database tables

---
+++ server/migrations/20190924065311-create_courses_table.js
@@ -0,0 +1,34 @@
+"use strict";
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    // Return promise
+    return queryInterface.createTable("course", {
+      id: {
+        type: Sequelize.INTEGER(10),
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      name: {
+        type: Sequelize.STRING(50),
+        allowNull: true
+      },
+      domain: {
+        type: Sequelize.STRING(100),
+        allowNull: true
+      },
+      description: {
+        type: Sequelize.STRING(100),
+        allowNull: true
+      },
+      createdAt: Sequelize.DATE,
+      updatedAt: Sequelize.DATE
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    //Return a promise that drops a table in case of (migration:undo)
+    return queryInterface.dropTable("course");
+  }
+};
---
+++ server/migrations/20190924070237-create_test_table.js
@@ -0,0 +1,37 @@
+"use strict";
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable("test", {
+      id: {
+        type: Sequelize.INTEGER(10),
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      course_id: {
+        type: Sequelize.INTEGER(10),
+        allowNull: false
+      },
+      num_of_questions: {
+        type: Sequelize.INTEGER(10),
+        allowNull: false
+      },
+      name: {
+        type: Sequelize.STRING(50),
+        allowNull: true
+      },
+
+      duration: {
+        type: Sequelize.STRING(10),
+        allowNull: true
+      },
+      createdAt: Sequelize.DATE,
+      updatedAt: Sequelize.DATE
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable("test");
+  }
+};
